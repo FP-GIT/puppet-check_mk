c65de3d45c578e4a4354d629904501ab58b981c5
Scharfenberg <c.scharfenberg@.francotyp.com>
2018-06-25 16:08:32 +0200

    removed package file name parsing

--- manifests/install.pp
+++ manifests/install.pp
@@ -15,21 +15,14 @@ class check_mk::install (
       source  => "${filestore}/${package}",
       require => File[$workspace],
     }
-    # omd-0.56-rh60-29.x86_64.rpm
-    if $package =~ /^(omd-\d+\.\d+)-(.*?)\.(rpm|deb)$/ {
-      $package_name = $1
-      $type         = $3
-      package { $package_name:
-        ensure   => installed,
-        provider => $type,
-        source   => "${workspace}/${package}",
-        require  => File["${workspace}/${package}"],
-      }
+    package { $package:
+      ensure   => installed,
+      source   => "${workspace}/${package}",
+      require  => File["${workspace}/${package}"],
     }
   }
   else {
-    $package_name = $package
-    package { $package_name:
+    package { $package:
       ensure => installed,
     }
   }
@@ -37,6 +30,6 @@ class check_mk::install (
   exec { 'omd-create-site':
     command => "/usr/bin/omd create ${site}",
     creates => $etc_dir,
-    require => Package[$package_name],
+    require => Package[$package],
   }
 }
