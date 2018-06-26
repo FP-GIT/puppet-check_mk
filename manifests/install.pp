727cabab0f20b23e46e7812b536ba095713b5289
Scharfenberg <c.scharfenberg@.francotyp.com>
2018-06-26 13:00:10 +0200

    now using deb_package module. So no rpm support any more.

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,35 +1,12 @@
 class check_mk::install (
   $site,
-  $workspace,
   $filestore = undef,
   $package = undef,
 ) {
+
   if $filestore {
-    if ! defined(File[$workspace]) {
-      file { $workspace:
-        ensure => directory,
-      }
-    }
-    file { "${workspace}/${package}":
-      ensure  => present,
-      source  => "${filestore}/${package}",
-      require => File[$workspace],
-    }
-    if $package =~ /^(.*)\.(rpm|deb)$/ {
-      $package_name = $1
-      $type         = $2
-      package { $package_name:
-        ensure   => installed,
-        provider => $type ? {
-          'deb' => 'dpkg',
-          'rpm' => 'rpm',
-        },
-        source   => "${workspace}/${package}",
-        require  => File["${workspace}/${package}"],
-      }
-    } else {
-      fail ("'${package_name}' neither sepcifies a deb nor a rpm package")
-    }
+    $package_name = "${filestore}/${package}"
+    deb_package{ $package_name: }
   }
   else {
     $package_name = $package
@@ -37,9 +14,9 @@ class check_mk::install (
       ensure => installed,
     }
   }
-  exec { 'omd-create-site':
+  ->exec { 'omd-create-site':
     command => "/usr/bin/omd create ${site}",
     unless  => "/usr/bin/omd sites | /bin/grep ${site}",
-    require => Package[$package_name],
   }
+
 }
