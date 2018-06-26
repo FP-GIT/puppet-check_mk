5228cda45ce959df61d4b7e6e6d9c30f95c9ec80
Scharfenberg <c.scharfenberg@.francotyp.com>
2018-06-26 15:38:52 +0200

    introduced proper boolean to string conversion

--- manifests/omd_config.pp
+++ manifests/omd_config.pp
@@ -1,10 +1,16 @@
 define check_mk::omd_config (
-  String $site,
-  String $setting = $title,
-  Data $value,
+  String                  $site,
+  String                  $setting = $title,
+  Variant[Scalar, Undef]  $value,
 ) {
 
-  $cmd = "/usr/bin/omd config ${site} set ${setting} '${value}'"
+  if $value =~ Boolean {
+    $value_string = bool2str($value, 'on', 'off')
+  } else {
+    $value_string = $value
+  }
+
+  $cmd = "/usr/bin/omd config ${site} set ${setting} ${value}"
   $check_cmd = "/usr/bin/omd config ${site} show ${setting}"
   exec { $cmd:
     onlyif => "/bin/bash -c \'[ `${check_cmd}` != \"${value}\" ]\'",
