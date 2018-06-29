86d43b6c1db8b2b1c9fd54708a19a028c368452e
carsten <genfer.barsch@posteo.de>
2018-06-29 16:48:40 +0200

    removed site/params.pp and moved everything to params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -22,4 +22,29 @@ class check_mk::params {
     }
   }
 
+  # Site specific params
+  $site_admin_mail              = undef
+  $site_autostart               = true
+  $site_core                    = 'nagios'
+  $site_crontab                 = true
+  $site_tmpfs                   = true
+  $site_apache_mode             = 'own'
+  $site_apache_tcp_address      = '127.0.0.1'
+  $site_apache_tcp_port         = 5000
+  $site_default_gui             = 'check_mk'
+  $site_dokuwiki_auth           = false
+  $site_multisite_authorisation = true
+  $site_multisite_cookie_auth   = true
+  $site_nagios_theme            = 'classicui'
+  $site_mkeventd                = true
+  $site_mkeventd_snmptrap       = false
+  $site_mkeventd_syslog         = false
+  $site_mkeventd_syslog_tcp     = false
+  $site_nagvis_urls             = 'check_mk'
+  $site_pnp4nagios              = true
+  $site_livestatus_tcp          = false
+  $site_livestatus_tcp_port     = 6557
+  $site_nsca                    = false
+  $site_nsca_tcp_port           = 5667
+
 }
